bfeaa69309ecd701d0864ae8dd6d061ee30b49a1
Karin Toth <karin.toth@exini.com>
2017-06-20 11:50:49 +0200

    ex 2: added first contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Contact, Address } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
+
+  public contact: Contact = {
+    id: 6,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  }
+
 }
